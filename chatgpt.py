490ce2fa5b8beb70538e6a6f99c3735d2452af2c
Peter van Doorn <71013416+two-trick-pony-NL@users.noreply.github.com>
2023-10-14 14:20:05 +0200

    basic function live

--- chatgpt.py
+++ chatgpt.py
@@ -1,10 +1,11 @@
+import streamlit as st
 import os
 import sys
 
 import openai
-from langchain.chains import ConversationalRetrievalChain, RetrievalQA
+from langchain.chains import ConversationalRetrievalChain
 from langchain.chat_models import ChatOpenAI
-from langchain.document_loaders import DirectoryLoader, TextLoader
+from langchain.document_loaders import DirectoryLoader
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.indexes import VectorstoreIndexCreator
 from langchain.indexes.vectorstore import VectorStoreIndexWrapper
@@ -16,37 +17,57 @@ import constants
 os.environ["OPENAI_API_KEY"] = constants.APIKEY
 
 # Enable to save to disk & reuse the model (for repeated queries on the same data)
-PERSIST = False
+PERSIST = True
 
-query = None
-if len(sys.argv) > 1:
-  query = sys.argv[1]
+# List files in the "data" folder
+data_folder = "data"
+data_files = [file for file in os.listdir(data_folder) if os.path.isfile(os.path.join(data_folder, file))]
 
-if PERSIST and os.path.exists("persist"):
-  print("Reusing index...\n")
-  vectorstore = Chroma(persist_directory="persist", embedding_function=OpenAIEmbeddings())
-  index = VectorStoreIndexWrapper(vectorstore=vectorstore)
-else:
-  #loader = TextLoader("data/data.txt") # Use this line if you only need data.txt
-  loader = DirectoryLoader("data/")
-  if PERSIST:
-    index = VectorstoreIndexCreator(vectorstore_kwargs={"persist_directory":"persist"}).from_loaders([loader])
-  else:
-    index = VectorstoreIndexCreator().from_loaders([loader])
+#Title
+st.title('🔥Docusearch GPT App')
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
 
-chain = ConversationalRetrievalChain.from_llm(
-  llm=ChatOpenAI(model="gpt-3.5-turbo"),
-  retriever=index.vectorstore.as_retriever(search_kwargs={"k": 1}),
-)
 
-chat_history = []
-while True:
-  if not query:
-    query = input("Prompt: ")
-  if query in ['quit', 'q', 'exit']:
-    sys.exit()
-  result = chain({"question": query, "chat_history": chat_history})
-  print(result['answer'])
 
-  chat_history.append((query, result['answer']))
-  query = None
+# Sidebar to display files
+st.sidebar.title("File List")
+st.sidebar.write(data_files)
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# Set a larger text input box
+query = st.chat_input("What do you want to know?:")
+if query:
+    with st.chat_message("user"):
+        st.markdown(query)
+        # Add user message to chat history
+        st.session_state.messages.append({"role": "user", "content": query})
+
+    if PERSIST and os.path.exists("persist"):
+        print("Reusing index...\n")
+        vectorstore = Chroma(persist_directory="persist", embedding_function=OpenAIEmbeddings())
+        index = VectorStoreIndexWrapper(vectorstore=vectorstore)
+    else:
+        loader = DirectoryLoader(data_folder)
+        if PERSIST:
+            index = VectorstoreIndexCreator(vectorstore_kwargs={"persist_directory": "persist"}).from_loaders([loader])
+        else:
+            index = VectorstoreIndexCreator().from_loaders([loader])
+
+    chain = ConversationalRetrievalChain.from_llm(
+        llm=ChatOpenAI(model="gpt-3.5-turbo"),
+        retriever=index.vectorstore.as_retriever(search_kwargs={"k": 1}),
+    )
+
+    chat_history = []
+    if query:
+        result = chain({"question": query, "chat_history": chat_history})
+        chat_history.append((query, result['answer']))
+        with st.chat_message("assistant"):
+          st.markdown(result["answer"])
+          # Add assistant response to chat history
+          st.session_state.messages.append({"role": "assistant", "content": result["answer"]})
